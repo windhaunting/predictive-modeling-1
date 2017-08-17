f3fc2ffead7d27c2298789ae806f731bbb24960b
fubao <freadream@gmail.com>
2017-08-16 20:47:01 -0400

    add_function

--- predictLR.py
+++ predictLR.py
@@ -8,6 +8,8 @@ Created on Wed Aug 16 20:03:30 2017
 
 #black friday purchase amount prediction
 #using linear regression
+from sklearn import linear_model
+import pandas as pd
 
 class predictLR:
  
@@ -16,17 +18,20 @@ class predictLR:
 
 
     def readInputData(self, inputFile):
+        df = pd.read_csv(inputFile)
+        print ("df: ", df)
+        
+    def trainModel(self):
         x = 1
         
         
         
-
+    
 
 def main():
-    
-    gcObj = graphCreationClass()
-    gcObj.createNodeIdPlaces()    
-    print ('len graphCreationClass edgelist: ', len(graphCreationClass.edgeList))
+    preLRObj = predictLR()
+    inputFile = "../input_data1/train.csv"
+    preLRObj.readInputData(inputFile)
     
 if __name__== "__main__":
   main()
