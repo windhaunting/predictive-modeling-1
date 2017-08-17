e5f43afa2dc289df1c033f0e8a7c0729e1be7b08
fubao <freadream@gmail.com>
2017-08-16 20:07:26 -0400

    add file

---
+++ predictLR.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 20:03:30 2017
+
+@author: fubao
+"""
+
+#black friday purchase amount prediction
+#using linear regression
+
+class predictLR:
+ 
+    def __init__(self):
+      pass
+
+
