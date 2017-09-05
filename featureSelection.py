f927e645695c4f88e9680315cf98a1a6a1607a8d
fubao <freadream@gmail.com>
2017-09-05 09:26:01 -0400

    add_function

--- featureSelection.py
+++ featureSelection.py
@@ -36,10 +36,11 @@ def featureSelectionFilterVariance01(df, threshold):
 def featureSelectionFilterCorrelation02(df, threshold):
     #get column list
     # 1st calculate the feature pair; 2nd use X -> y;   df contains X and y
-    y = df.iloc[:,-1]       #y column
+    print ("featureSelectionFilterCorrelation02 df column", df.columns, len(df.columns))
+    y = df['Purchase']      # keep the column name df.iloc[:,-1]       #y column
     #dfX = df.iloc[:, :-1]      #create a view , not to delete;  df.drop(df.columns[[-1,]], axis=1, inplace=True)
     #df.drop(df.columns[[-1,]], axis=1, inplace=True) df.drop(df.index[2])
-
+    
     df.drop(df.columns[-1], axis=1, inplace=True)         
     correlations = {}
     columns = df.columns.tolist()
@@ -59,8 +60,8 @@ def featureSelectionFilterCorrelation02(df, threshold):
     colLsts = [f.split("__") for f in dfCorr.index.tolist()]
     cols = list(set(chain(*colLsts)))
     print ("cols:    ", len(cols))
-    df = df[cols]
-    print("featureSelectionFilterCorrelation02 df: ", df.shape)
+    df = pd.concat([df[cols], y], axis=1)
+    print("featureSelectionFilterCorrelation02 df: ", df.shape, y) #df['Purchase'])
     
 #use mutual information to do feature selection.
 #calculate all feature pairs with normalized mutual information(NMI); too cost for big feature set
